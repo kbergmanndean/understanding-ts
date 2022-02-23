d8deb568b8a0786a0a1f4de479f1da3ebfc5a2f7
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-02-23 14:59:44 -0500

    finish combine page

--- app.ts
+++ app.ts
@@ -1,16 +1,12 @@
-function combining (num1:number | string, num2:number | string, resultConversion: string) {
+type Combine = number | string
+
+function combining (num1:Combine, num2:Combine, resultConversion: string) {
     let result;
-   if (typeof num1 == 'number' && typeof num2 == 'number') {
-       result = num1 + num2
+   if (typeof num1 == 'number' && typeof num2 == 'number' || resultConversion == 'as-number') {
+       result = +num1 + +num2
    } else {
        result = num1.toString() + num2.toString()
    }
-   if (resultConversion == "as-number") {
-       return +result;
-   } else {
-       return result.toString()
-   }
-    
 }
 
 
