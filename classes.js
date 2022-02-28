3c78210f407e93eaa0882814eb77f4c00c4fa51a
kbergmanndean <82288836+kbergmanndean@users.noreply.github.com>
2022-02-28 16:44:49 -0500

    added employees

--- classes.js
+++ classes.js
@@ -1,7 +1,18 @@
 "use strict";
 class Store {
     constructor(n) {
+        this.employees = [];
         this.name = n;
+    }
+    describe() {
+        console.log('Store' + this.name);
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployees() {
+        console.log(this.employees);
     }
 }
 const target = new Store('target');
+target.describe();
--- classes.ts
+++ classes.ts
@@ -1,8 +1,24 @@
 class Store {
     name: string;
+
+    employees: string[]=[];
+
     constructor(n:string) {
         this.name = n;
+    }
+
+    describe() {
+        console.log('Store' + this.name);
+    }
+
+    addEmployee(employee:string) {
+        this.employees.push(employee);    
+    }
+
+    printEmployees() {
+        console.log(this.employees);
     }
 }
 
 const target = new Store('target');
+target.describe();
